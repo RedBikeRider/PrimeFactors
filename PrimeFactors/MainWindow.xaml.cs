575c9accd36a1f70c77121de2ac5f6213cfdfef4
Clinton <Clinton@DESKTOP-BTNL7BS>
2022-06-29 08:02:06 +0100

    Add project files.

---
+++ PrimeFactors/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PrimeFactors.Models;
+
+namespace PrimeFactors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly IList<PrimeFactorisation> _factors = new List<PrimeFactorisation>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public void BtnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            ok = int.TryParse(entered, out toFactorise);
+
+            if (ok && toFactorise >= 2)
+            {
+                StringBuilder factorList = new StringBuilder();
+
+                foreach (int factor in GetPrimeFactors(toFactorise, eratosthenes))
+                {
+                    if (factorList.Length == 0)
+                        factorList.Append("Prime factor(s): ");
+                    else
+                        factorList.Append(", ");
+                    factorList.Append(factor);
+                }
+
+                Console.WriteLine(factorList);
+            }
+
+
+        }
+
+        private static IEnumerable<int> GetPrimeFactors(int value, Eratosthenes eratosthenes)
+        {
+            List<int> factors = new List<int>();
+
+            foreach (int prime in eratosthenes)
+            {
+                while (value % prime == 0)
+                {
+                    value /= prime;
+                    factors.Add(prime);
+                }
+
+                if (value == 1)
+                    break;
+            }
+
+            return factors;
+        }
+    }
+}
---
+++ PrimeFactors/Models/PrimeFactorisation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors.Models
+{
+    public class PrimeFactorisation : INotifyPropertyChanged
+    {
+        public int ToFactorise { get; set; }
+        public List<int> PrimesList = new List<int>();
+        public bool IsChanged { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (propertyName != nameof(IsChanged))
+            {
+                IsChanged = true;
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
+        }
+    }
+}
